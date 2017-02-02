6a54af9bb74f20cd7878127b4f9fc818dd806bb0
mandersen21 <mikkelandersen21@gmail.com>
2017-02-02 22:53:08 +0100

    change some files.

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
-  res.sendFile('index.html', {root: path.join(__dirname, './files')});
+  res.sendFile(__dirname + '/index.html');
 })
 
 app.listen(3000, function () {
