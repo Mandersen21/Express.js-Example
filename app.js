cec69bb2d9544fcbcca336ed6590af8253138545
mandersen21 <mikkelandersen21@gmail.com>
2017-02-02 23:11:12 +0100

    Added view engine.

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var path = require('path');
+var cons = require('consolidate');
 var app = express();
 
 app.use(express.static('public'));
 
+// view engine setup
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 })
 
 app.listen(3000, function () {
